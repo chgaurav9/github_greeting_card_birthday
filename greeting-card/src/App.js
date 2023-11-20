09549a1201cc7a0149c5b5ebd46c98e6ed50b2b5
gaurav_kog <gaurav.chaudhary@kognitivinc.com>
2023-11-20 19:27:14 +0530

    Deployed website v2

--- greeting-card/src/App.js
+++ greeting-card/src/App.js
@@ -1,12 +1,113 @@
 import React, { useState, useEffect } from 'react';
 import { useSpring, animated, config } from 'react-spring';
-import gsap from 'gsap'; 
 import './App.css';
 
 const confettiColors = ['#ffcc00', '#ff6699', '#99ff99', '#3399ff', '#ff9966'];
 
+const images = [
+  '/IMG_2673.jpg',
+  '/IMG_2681.jpg',
+  '/IMG_7144.jpg',
+  '/IMG_7180.jpg',
+  '/IMG_7181.jpg',
+  '/IMG_7234.jpg',
+  '/IMG_7235.jpg',
+  '/IMG_7236.jpg',
+  '/IMG_7375.jpg',
+  '/IMG_7376.jpg',
+  '/IMG_7380.jpg',
+  '/IMG_7388.jpg',
+  '/IMG_7471.jpg',
+  '/IMG_7512.jpg',
+  '/IMG_7603.jpg',
+  '/IMG_7609.jpg',
+  '/IMG_7614.jpg',
+  '/IMG_7624.jpg',
+  '/IMG_7858.jpg',
+  '/IMG_7860.jpg',
+  '/IMG_7873.jpg',
+  '/IMG_7912.jpg',
+  '/IMG_7913.jpg',
+  '/IMG_8104.jpg',
+  '/IMG_8197.jpg',
+  '/IMG_8199.jpg',
+  '/IMG_8201.jpg',
+  '/IMG_8216.jpg',
+  '/IMG_8308.jpg',
+  '/IMG_8319.jpg',
+  '/IMG_8399.jpg',
+  '/IMG_8404.jpg',
+  '/IMG_8438.jpg',
+  '/IMG_8494.jpg',
+  '/IMG_8496.jpg',
+  '/IMG_8584.jpg',
+  '/IMG_8680.jpg',
+  '/IMG_8687.jpg',
+  '/IMG_8841.jpg',
+  '/IMG_8921.jpg',
+  '/IMG_8947.jpg',
+  '/IMG_8982.jpg',
+  '/IMG_9029.jpg',
+  '/IMG_9038.jpg',
+  '/IMG_9114.jpg',
+  '/IMG_9155.jpg',
+  '/IMG_9167.jpg',
+  '/IMG_9232.jpg',
+  '/IMG_9238.jpg',
+  '/IMG_9253.jpg',
+  '/logo192.png',
+  // Additional images
+  '/PHOTO-2023-07-16-21-56-52.jpg',
+  '/PHOTO-2023-07-16-21-58-42.jpg',
+  '/PHOTO-2023-07-16-22-01-42.jpg',
+  '/PHOTO-2023-07-23-19-33-26.jpg',
+  '/PHOTO-2023-08-14-21-59-48.jpg',
+  '/PHOTO-2023-08-15-16-11-21.jpg',
+  '/PHOTO-2023-08-16-16-00-23.jpg',
+  '/PHOTO-2023-08-23-00-57-02.jpg',
+  '/PHOTO-2023-08-23-00-57-27.jpg',
+  '/PHOTO-2023-08-23-22-39-57.jpg',
+  '/PHOTO-2023-08-29-00-15-13.jpg',
+  '/PHOTO-2023-08-29-00-15-21.jpg',
+  '/PHOTO-2023-08-29-00-15-29.jpg',
+  '/PHOTO-2023-08-29-21-28-23.jpg',
+  '/PHOTO-2023-09-07-22-21-37.jpg',
+  '/PHOTO-2023-09-07-22-21-38 2.jpg',
+  '/PHOTO-2023-09-07-22-21-38.jpg',
+  '/PHOTO-2023-09-07-22-26-07.jpg',
+  '/PHOTO-2023-09-12-13-42-03.jpg',
+  '/PHOTO-2023-09-12-13-44-01.jpg',
+  '/PHOTO-2023-09-12-13-59-14.jpg',
+  '/PHOTO-2023-09-12-14-00-26.jpg',
+  '/PHOTO-2023-09-12-14-06-24.jpg',
+  '/PHOTO-2023-09-12-14-09-44.jpg',
+  '/PHOTO-2023-09-12-14-10-31.jpg',
+  '/PHOTO-2023-09-12-14-15-41.jpg',
+  '/PHOTO-2023-09-12-14-17-25.jpg',
+  '/PHOTO-2023-09-16-21-35-57.jpg',
+];
+
+
+// Shuffle function for arrays
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 function App() {
   const [isBirthday, setIsBirthday] = useState(true);
+  const [compliment, setCompliment] = useState('');
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [showApologyButton, setShowApologyButton] = useState(false);
+
+  // Shuffle images and compliments on component mount
+  useEffect(() => {
+    shuffleArray(images);
+    shuffleArray(compliments);
+  }, []);
 
   const cardAnimation = useSpring({
     opacity: 1,
@@ -28,15 +129,114 @@ function App() {
     }
   }, [isBirthday]);
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []); // Run this effect only once on component mount
+
   const animateBalloons = () => {
     const balloons = document.querySelectorAll('.balloon');
+  };
 
-    gsap.to(balloons, {
-      y: -50, // Adjust this value for the height of the balloon animation
-      duration: 2,
-      stagger: 0.2, // Time delay between each balloon animation
-      ease: 'elastic.out(1, 0.3)', // Use your preferred easing function
-    });
+
+
+  const compliments = [
+    `You are amazing!`,
+    `I appreciate you so much!`,
+    `You make every day special.`,
+    `You have a heart of gold.`,
+    `Your kindness knows no bounds.`,
+    `You always bring a smile to my face.`,
+    `Your positive energy is contagious.`,
+    `You're a true gem.`,
+    `Your presence makes a difference.`,
+    `You radiate beauty, inside and out.`,
+    `You're a beacon of light.`,
+    `You have a wonderful sense of humor.`,
+    `Your strength is admirable.`,
+    `You're a fantastic friend.`,
+    `You're so thoughtful and considerate.`,
+    `You're a joy to be around.`,
+    `Your resilience is inspiring.`,
+    `You're a true blessing in my life.`,
+    `Your generosity knows no limits.`,
+    `You're a ray of sunshine.`,
+    `Your positive attitude is uplifting.`,
+    `You're incredibly talented.`,
+    'You have a heart as big as the ocean.',
+    'Your smile lights up the room.',
+    'You make the world a better place.',
+    `You're a true friend in every sense.`,
+    `Your passion is contagious.`,
+    `You're a remarkable person.`,
+    `You make a significant impact.`,
+    `Your dedication is commendable.`,
+    `You're a beautiful soul.`,
+    `Your presence brings joy to others.`,
+    `You're one in a million.`,
+    `You have a gift for making people feel valued.`,
+    `You're a beacon of positivity.`,
+    `You have an incredible work ethic.`,
+    `You're a true example of kindness.`,
+    `Your perseverance is admirable.`,
+    `You're a genuine and authentic person.`,
+    `You have a heart full of love.`,
+    `Your friendship means the world to me.`,
+    `You're an absolute delight.`,
+    `You handle challenges with grace.`,
+    `You're a source of inspiration for many.`,
+    `Your generosity knows no bounds.`,
+    `You're a beautiful person, inside and out.`,
+    `You have a heart of gold.`,
+    `You make a positive impact on everyone you meet.`,
+    `You're a fantastic listener.`,
+    `Your compassion knows no limits.`,
+    `You're a true gift to those around you.`,
+    `You have an incredible spirit.`,
+    `You're a source of strength for others.`,
+    `You're a true friend through thick and thin.`,
+    `Your positive outlook is contagious.`,
+    `You're an absolute treasure.`,
+    `You have an unwavering resolve.`,
+    `You're a beacon of hope for others.`,
+    `Your sincerity is refreshing.`,
+    `You're a true original.`,
+    `You make a difference in the world.`,
+    `Your creativity knows no bounds.`,
+    `You're a pillar of strength.`,
+    `You have a heart full of love and kindness.`,
+    `You're a true team player.`,
+    `Your generosity is inspiring.`,
+    `You're a constant source of positivity.`,
+    `You're an extraordinary person.`,
+    `Your humility is truly admirable.`,
+    `You're a shining example of kindness.`,
+    `You have a magnetic personality.`,
+    `You're a source of inspiration for many.`,
+    `Your dedication is truly commendable.`,
+    `You're a beacon of light in challenging times.`,
+    `You have an incredible ability to bring people together.`,
+    `You're a true role model.`,
+    `Your selflessness is truly remarkable.`,
+    `You're a source of joy and happiness.`,
+    `Your kindness leaves a lasting impact.`,
+    `You're an absolute joy to be around.`,
+    `You have an incredible way of making people feel seen and heard.`,
+    `You're a true force for good in the world.`,
+    `Your optimism is contagious.`,
+    `You're a ray of sunshine on cloudy days.`,
+    `You have a heart that's as big as the sky.`,
+    `You're a beacon of positivity in the world.`
+
+  ];
+
+
+  const handleCompliment = () => {
+    const randomCompliment = compliments.pop();
+    setCompliment(randomCompliment);
   };
 
   return (
@@ -50,19 +250,28 @@ function App() {
         </p>
         {isBirthday && (
           <>
-            <animated.div style={confettiAnimation} className="confetti-container">
-              {confettiColors.map((color, index) => (
-                <div key={index} className="confetti" style={{ backgroundColor: color }} />
-              ))}
-            </animated.div>
+            {compliment && (
+              <p className="compliment" style={{ marginBottom: '5px' }}>
+                <strong>{compliment}</strong>
+              </p>
+            )}
+            <div style={{ marginTop: '10px', marginBottom: '10px' }}>
+              <button onClick={handleCompliment}>Give a Compliment</button>
+            </div>
             <div className="cake-container">
-              <img src={process.env.PUBLIC_URL + '/IMG_9253.jpg'} alt="Birthday Cake" className="cake-image" />
+              <img src={process.env.PUBLIC_URL + images[currentImageIndex]} alt="Birthday Cake" className="cake-image" />
             </div>
           </>
         )}
-        <button onClick={() => setIsBirthday(!isBirthday)}>
-          {isBirthday ? 'Switch to Apology' : 'Switch to Birthday'}
-        </button>
+        {showApologyButton && (
+          <button onClick={() => setIsBirthday(!isBirthday)}>
+            {isBirthday ? 'Switch to Apology' : 'Switch to Birthday'}
+          </button>
+        )}
+        <label>
+
+          <input type="checkbox" checked={showApologyButton} onChange={() => setShowApologyButton(!showApologyButton)} />
+        </label>
       </animated.div>
     </div>
   );
